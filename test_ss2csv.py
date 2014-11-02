fb643a53e64cd829dad340a4c3b4764f58a7c882
bdowns <brian.downs@gmail.com>
2014-11-02 00:49:34 -0700

    update test

--- test_ss2csv.py
+++ test_ss2csv.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
+import sys
+from ss2csv import *
 
+#
+# Tests
+#
+
+try:
+    import xlrd
+except ImportError, xlrd_error:
+    print xlrd_error.message
+    sys.exit(1)
+
+def test_is_spreadsheet(file):
+    assert is_spreadsheet(file)
