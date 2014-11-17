5061b4d3120f0d6e7aa3f39f8afeae0342476627
Downs <bdowns@io.com>
2014-11-17 11:24:12 -0700

    updated tests

--- test_ss2csv.py
+++ test_ss2csv.py
@@ -13,6 +13,9 @@ test_file = 'test_ss.xlsx'
 
 
 def test_xlrd_installation():
+    """
+    Make sure the xlrd module is installed and can be loaded.
+    """
     try:
         imp.find_module('xlrd')
         assert True
@@ -21,17 +24,32 @@ def test_xlrd_installation():
 
 
 def test_is_spreadsheet():
+    """
+    Test that the given test file is actually a spreadsheet
+    """
     assert is_spreadsheet(test_file)
 
 
 def test_workboot_data():
+    """
+    Test to verify a workbook obj can be created from the given
+    test file
+    """
     wb = WorkbookData(test_file)
     assert isinstance(wb, WorkbookData)
+
+    # Verify the given workbook has at least 1 worksheet.  If not,
+    # something went really wrong
     assert wb.worksheet_count >= 1
 
 
 def test_save_to_csv():
+    """
+    Test to validate a csv file can be saved from the xlsx.
+    """
     wb = WorkbookData(test_file)
     s = SaveToCSV(wb)
+
+    # Make sure that the built instance is what it should be
     assert isinstance(s, threading.Thread)
     assert isinstance(s, SaveToCSV)
